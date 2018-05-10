618c2e4b096268a4f1d06f1f52c5eff140906b3d
david <davidvoler@gmail.com>
2018-05-10 11:09:27 +0300

    adding react-redux thunk and material-ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>HDIS Hakathon</h1>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
