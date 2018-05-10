b19f571512ee150405160b5e6858fc7126af89e6
david <davidvoler@gmail.com>
2018-05-10 11:14:51 +0300

    initial app with bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
-
+import AppBarExampleIconButton from './components/layout/bar'
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
       <div className="App">
-        <h1>HDIS Hakathon</h1>
+        <AppBarExampleIconButton />
       </div>
       </MuiThemeProvider>
     );
