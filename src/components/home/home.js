85443fc82c0b2c23c18413d476d36d5cb1c4b582
david <davidvoler@gmail.com>
2018-05-11 08:56:46 +0300

    added code to home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { requestLessonList } from '../../actions/editor';
+
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+import ReactPlayer from 'react-player';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Home extends React.Component {
-	render() {
-		return (
+  render() {
+    return (
       <div>
-          <h2> Home </h2>
+        <h2> Home </h2>
+
+        <List>
+          {this.props.list.lessons.map(lesson => <ListItem key={lesson.id}>
+            <ReactPlayer url={lesson.videoURL} />
+            <RaisedButton > Purchase</RaisedButton>
+          </ListItem>)}
+        </List>
       </div>
-		);
-	}
-    componentWillMount() {
-        console.log("componentDidMount")
-    }
+    );
+  }
+  componentWillMount() {
+    this.props.fetchLessons();
+  }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  list: state.editor.list
+});
 const mapDispatchToProps = (dispatch) => {
-    return { };
+  return {
+    fetchLessons: () => dispatch(requestLessonList({}))
+  };
 };
-export default connect(mapStateToProps,mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
